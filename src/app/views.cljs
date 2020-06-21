c423aa84dde6fb6f10e480d535082c3b005fe2b4
ngygbr <ngabesz20@icloud.com>
2020-06-21 20:23:51 +0200

    elso

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,16 +1,37 @@
 (ns app.views
-  (:require [app.state :refer [state]]))
+  (:require [reagent.core :refer [atom]]))
 
-(defn increment []
-  (swap! state update-in [:count] inc))
+(defonce tasks (atom '({:1 "Teszt" :2 false} {:1 "Alma" :2 false} {:1 "Körte" :2 false} {:1 "Barack" :2 false})))
 
-(defn decrement []
-  (swap! state update-in [:count] dec))
+(defn atom-input [value]
+    [:input {:type "text"
+             :value @value
+             :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn add [value]
+  (swap! tasks conj {:1 @value :2 false})
+  (reset! value ""))
+
+
+(defn clear-all []
+    (reset! tasks '()))
+
+(defn delete-task [value]
+    (swap! tasks (remove (nth @tasks @value) tasks)))
+
+;(defn done [value])
+
 
 (defn app []
-  [:div
-   [:h1.title "Simple Webapp"]
-   [:div.wrapper
-    [:button.btn {:on-click #(decrement)} "-"]
-    [:p.counter (get @state :count)]
-    [:button.btn {:on-click #(increment)} "+"]]])
+  (let [val (atom "")]
+    (fn []
+      [:div
+       [:h1 "TODO WEBAPP"]
+       [:p [atom-input val]]
+       [:button.btn1 {:on-click #(add val)} "Add"]
+       [:button.btn1 {:on-click #(clear-all)} "Clear"]
+       [:p.counter "Tasks: " (count @tasks)]
+       [:p @tasks]
+       (for [x (range (count @tasks))]
+        (let [task (nth @tasks x)]
+          [:p [:input {:type "checkbox" :on-click #() }] (task :1) [:button.btn2 {:on-click #(delete-task x) } "Delete"]]))])))
